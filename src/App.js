a2b816d52c383c0a9a2f6772d88470ede1f37009
Santiago Torres <santiagotorresredecilla@gmail.com>
2020-03-05 16:04:20 +0200

    getting started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Box from '@material-ui/core/Box';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+import CheckBox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import calculator from './Calculator';
 import './App.css';
 
 function App() {
-  return null; 
+  return (
+    <div className="App">
+      <Box id="exe1">
+        
+      </Box>
+    </div>
+  ); 
 }
 
 export default App;
