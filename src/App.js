fb5ce495cd3a986a525bb83c198e1391ae546351
Santiago Torres <santiagotorresredecilla@gmail.com>
2020-03-18 16:44:16 +0200

    3 done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import './App.css';
-import Checkbox from '@material-ui/core/Checkbox';
-import { gray } from 'color-name';
-import { withStyles } from '@material-ui/core/styles';
-import RadioButtonCheckedTwoToneIcon from '@material-ui/icons/RadioButtonCheckedTwoTone';
-import RadioButtonUncheckedTwoToneIcon from '@material-ui/icons/RadioButtonUncheckedTwoTone';
-import { TextareaAutosize } from '@material-ui/core';
 
-const GrayCheckbox = withStyles({
-  root: {
-    color: gray[400],
-    '&$checked': {
-      color: gray[600],
-    },
-  },
-  checked: {},
-})(props => <Checkbox color="default" {...props} />);
+
 
 function App() {
   const [mCilck, setMClick] = React.useState(0);
-  const [mMove, setMMove] = React.useState(0);
+  const [mMove, setMMove] = React.useState({
+    x: 0,
+    y: 0
+  });
   const [mScrol, setMScrol] = React.useState(0);
 
   
@@ -31,16 +18,24 @@ function App() {
     setMClick(mCilck + 1);
   }
 
+  const handleMouse = event => {
+    setMMove({ x: event.screenX,
+               y: event.screenY });
+  }
+  const handleScrol = event => {
+    setMScrol(event.deltaY);
+  }
 
   return (
-    <div className="App">
-      <Box id="exe1" border={1} width={1/4} padding={3}>
+    <div className="App" style={{height:900, width:600}} onMouseMove={handleMouse} onWheel={handleScrol}>
+      <Box id="exe1" border={1} width={1/2} padding={3}>
         <Box border={1}>Mouse was clicked:
             <Button variant="outlined" onClick={handleClick}> Button {mCilck}</Button>
         </Box>
         <br></br>
-        <Box  border={1} height={30}></Box>
-      
+        <Box  border={1} height={30}>Mouse was moved at x: {mMove.x} y: {mMove.y}</Box>
+        <br></br>
+        <Box  border={1} height={30}>Mouse wheel event: {mScrol}</Box>  
          
       </Box>
     </div>
