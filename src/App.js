47ecafa6f28e7b9c96909d1070c061bc48db1663
santiagt <santiagotorresredecilla@gmail.com>
2020-04-25 11:29:11 +0300

    chart added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles } from '@material-ui/core';
-import { Chart } from 'react-charts'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles, TextField } from '@material-ui/core';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
 
 
 const useStyle = makeStyles (theme => ({
@@ -24,32 +31,46 @@ const useStyle = makeStyles (theme => ({
 
 
 function App() {
+  const data = [
+    {name: 'Page A', pv: 2400},
+    {name: 'Page B', pv: 1398},
+    {name: 'Page C', pv: 9800},
+    {name: 'Page D', pv: 3908},
+    {name: 'Page E', pv: 4800},
+    {name: 'Page F', pv: 3800},
+    {name: 'Page G', pv: 4300},
+];
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
 
-  const data = React.useMemo(
-    () => [
-      {
-        label: 'data1',
-        data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
-      },
-      {
-        label: 'data2',
-        data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
-      }
-    ]
-  )
-  const series = React.useMemo(
-    () => ({
-      showPoints: true
-    }),
-    []
-  )
-  const axes = React.useMemo(
-    () => [
-      { primary: true, type: 'linear', position: 'bottom' },
-      { type: 'linear', position: 'left' }
-    ],
-    []
-  )
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+ 
 
 // useEffect(() => {
 
@@ -64,32 +85,59 @@ function App() {
             <MenuIcon />
          </IconButton>
         <Button color="inherit">File</Button>
-        <Button color="inherit">Edit</Button>
           
         </Toolbar>
       </AppBar>
       <div>
-        <>
         
-        {/* <Box width={1/2} padding={5} 
+        <Box width={1/2} padding={5} 
         paddingBottom={10}  
-        className={ styledClasses.box1 }> */}
-          <Box
-            style={{
-              background: 'rgba(0, 27, 45, 0.9)',
-              padding: '.5rem',
-              borderRadius: '5px'
-            }}
-          >
-            <Chart data={data} series={series} axes={axes} tooltip />
+        className={ styledClasses.box1 }>
+          <Typography variant="h5" align='center'>Exchange rate (USD)</Typography>
+          <br></br>
+          
+          <LineChart width={600} height={300} data={data}
+          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <Tooltip/>
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+          </LineChart>
+          <TextField></TextField>
+          <div>
+            <div>
+              <Button
+                ref={anchorRef}
+                aria-controls={open ? 'menu-list-grow' : undefined}
+                marginLeft={90}
+                aria-haspopup="true"
+                onClick={handleToggle}
+              >
+                currency
+              </Button>
+              <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                  >
+                    <Paper>
+                      <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                          <MenuItem onClick={handleClose}>Profile</MenuItem>
+                          <MenuItem onClick={handleClose}>My account</MenuItem>
+                          <MenuItem onClick={handleClose}>Logout</MenuItem>
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                  )}
+                </Popper>
+            </div>
+          </div>
         </Box>
-      <br />
-        {/* </Box>
-         */}
-
-        </>
       </div>
-    
     </div>
   ); 
 }
